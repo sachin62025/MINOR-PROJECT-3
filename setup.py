cf03acfc5ecb1b0290ffe5db53edc7e9a81499a0
sachin Kumar <titan18449@gmail.com>
2025-06-14 12:36:53 +0530

    'done'

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 setup(
     name="MLOPS-PROJECT-8",
     version="0.1",
-    author="Sudhanshu",
+    author="sachin",
     packages=find_packages(),
     install_requires = requirements,
 )
